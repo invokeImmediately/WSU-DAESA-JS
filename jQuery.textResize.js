b89acd40eb1c75f026fed9c60ca99d85d79b3e41
Daniel Rieck <danielcrieck@gmail.com>
2015-10-29 10:49:13 -0700

    Added jQuery.textResize.js and made file naming consistent

---
+++ jQuery.textResize.js
@@ -0,0 +1,45 @@
+/**
+ * jQuery.textResize.js
+ * Released under GNU GPLv2
+ *
+ * Based on FitText.js 1.2 (https://github.com/davatron5000/FitText.js) by Dave Rupert
+ *  (http://daverupert.com).
+ */
+(function($){
+    $.fn.textResize = function( scalingFactor, options ) {
+        // Set up default options in case the caller passed no attributes
+        var scalingAmount = scalingFactor || 1,
+            settings = $.extend({
+                'minFontSize' : Number.NEGATIVE_INFINITY,
+                'maxFontSize' : Number.POSITIVE_INFINITY
+            }, options);
+        return this.each(function () {
+            var $this = $(this);
+          
+            // Resizer() keeps font-size proportional to object width as constrainted by the user
+            var resizer = function () {
+                $this.css('font-size', Math.max(Math.min($this.width() / (scalingAmount*10),
+                    parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+            };
+          
+            // Call once to set the object's font size based on current window size, then call as
+            // resize or orientation-change events are triggered.
+            textResizer();
+            $(window).on('resize.textresize orientationchange.textresize', textResizer);
+        });
+    };
+    
+    // Now use the plugin on the WSU Undergraduate education website (i.e. delete or modify the
+    // following statement if you are going to utilize this plugin on your own site).
+    $(document).ready(function () {
+        $('section.article-header div.header-content h1').each(function () {
+            $(this).textResizer(1.277142857142857);
+        });
+        $('section.article-header div.header-content h2').each(function () {
+            $(this).textResizer(1.847840465639262);
+        });
+        $('section.article-header div.header-content h3').each(function () {
+            $(this).textResizer(4.110097222222222);
+        });
+    );
+})(jQuery);

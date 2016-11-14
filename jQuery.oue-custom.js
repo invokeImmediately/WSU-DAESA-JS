caba983b8863f082217ff1ecd687efc019a2bafa
Daniel Rieck <danielcrieck@gmail.com>
2016-11-14 08:37:10 -0800

    Debugging semantic error in selector syntax
    Adding missing pound sign to select by ID.

--- jQuery.oue-custom.js
+++ jQuery.oue-custom.js
@@ -98,7 +98,7 @@ function isJQuery($obj) {
 		// initScrollingSidebars("...");
         initWelcomeMessage(
 			"#welcome-message",
-			"post-welcome-message",
+			"#post-welcome-message",
 			1000,
 			500,
 			500

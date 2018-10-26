c55a95340df16e8cf325c1450298ca785c73a3aa
Daniel Rieck <danielcrieck@gmail.com>
2018-10-25 23:34:44 -0700

    Add class CountdownTimerSelectors
    Add a class for providing validated settings for OUE countdown timers that are functionalized through jQuery class selectors.

--- jQuery.countdown-custom.js
+++ jQuery.countdown-custom.js
@@ -12,16 +12,16 @@
 ( function ( $ ) {
 
 var $countdownTimerById;
+var $countdownTimersByClass;
 var animationTiming = 400;	// Controls the speed at which jQuery-induced countdown animations occur
 var thisFileName = 'jQuery.countdown-custom';
-
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // DOM-READY EXECUTION SECTION
 
 $( function () {
 	$countdownTimerById = processCountdownTimerById( '#countdown-clock' );
-	// $countdownClockByClass = $( '.countdown-clock' );
+	// TODO: $countdownTimersByClass = processCountdownTimersByClass( … );
 } );
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -29,13 +29,102 @@ $( function () {
 
 $( window ).on( 'load', function () {
 	showIdSelectedCountdownTimer( $countdownTimerById, animationTiming );
+	// TODO: showClassSelectedCountdownTimers( $countdownTimersByClass );
 } );
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-// IIFE-LOCALIZED DEFINITIONS OF FUNCTIONS USED IN THIS SCRIPT
+// CLASS DEFINITIONS
 
 /**
- * Find and process a countdown timer element that has been marked with an ID attribute.
+ * Validated settings for OUE countdown timers designed to be functionalized via jQuery class
+ * selectors.
+ *
+ * @param {string} timer - Class selector for isolating countdown timer elements.
+ * @param {string} container - Class selector for obtaining the container component of a countdown
+ *     timer.
+ * @param {string} message -  Class selector targeting the message component of a countdown timer.
+ * @param {string} prependedHtml - Class selector for isolating the component of a countdown timer
+ *     that contains HTML to be prepended to the inner HTML of the message.
+ * @param {string} appendedHtml - Class selector for isolating the component of a countdown timer
+ *     that contains HTML to be appended to the inner HTML of the message.
+ *
+ * @throws {object} If the type of any argument is invalid and/or an argument is not a properly
+ *     formatted class selector, an object will be thrown containing four string-typed parameters:
+ *     fileName, fName, fDesc, and errorMsg. The thrown object is thus meant to be utilized with the
+ *     jQuery.logError function.
+ */
+function CountdownTimerSelectors( timer, container, message, prependedHtml, appendedHtml ) {
+	////////////////////////////////////////////////////////////////////////////////////////////
+	// PRIVATE PROPERTIES
+
+	var argTypeMask = 0;
+	var validSelectorNeedle = /\.[a-zA-Z0-9\-_]+$/;
+	var validSelectorMask = 0;
+	var error = {
+		fileName: thisFileName,
+		fName: 'CountdownTimerSelectors',
+		fDesc: 'Validated settings for class-based OUE countdown timers.',
+		errorMsg: ''
+	};
+
+	////////////////////////////////////////////////////////////////////////////////////////////
+	// PUBLIC PROPERTIES
+
+	this.timer = timer;
+	this.container = container;
+	this.message = message;
+	this.prependedHtml = prependedHtml;
+	this.prependedHtml = appendedHtml;
+
+	////////////////////////////////////////////////////////////////////////////////////////////
+	// MAIN CONSTRUCTOR EXECUTION
+
+	checkArgTypes();
+	checkSelectorStructure();
+	throwAnyErrors();
+
+	////////////////////////////////////////////////////////////////////////////////////////////
+	// MAIN CONSTRUCTOR EXECUTION
+
+	function checkArgTypes() {
+		argTypeMask = typeof timer === 'string';
+		argTypeMask = ( argTypeMask << 1 ) | typeof container === 'string';
+		argTypeMask = ( argTypeMask << 1 ) | typeof message === 'string';
+		argTypeMask = ( argTypeMask << 1 ) | typeof prependedHtml === 'string';
+		argTypeMask = ( argTypeMask << 1 ) | typeof appendedHtml === 'string';
+	}
+
+	function checkSelectorStructure() {
+		validSelectorMask = !!validSelectorNeedle.exec( timer );
+		validSelectorMask = ( validSelectorMask << 1 ) | !!validSelectorNeedle.exec( container );
+		validSelectorMask = ( validSelectorMask << 1 ) | !!validSelectorNeedle.exec( message );
+		validSelectorMask = ( validSelectorMask << 1 ) |
+			!!validSelectorNeedle.exec( prependedHtml );
+		validSelectorMask = ( validSelectorMask << 1 ) | !!validSelectorNeedle.exec( appendedHtml );
+	}
+
+	function throwAnyErrors() {
+		if ( !argTypeMask || !validSelectorMask ) {
+			if ( !argTypeMask ) {
+				error.errorMsg = 'I encountered a wrongly typed argument during construction.';
+				if ( !validSelectorMask ) {
+					error.errorMsg += ' Also, ';
+				}
+			}
+			if ( !validSelectorMask ) {
+				error.errorMsg += 'I found that at least one of my arguments did not contain a prop\
+erly formed class selector as required.';
+			}
+			throw error;
+		}
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// FUNCTION DEFINITIONS
+
+/**
+ * Find and process a countdown timer element that has been marked with the ID attribute.
  * 
  * @param {string} selectorStr - Selector string for isolating countdown timer elements within the
  *     DOM.
@@ -149,6 +238,18 @@ e\'s information on the variables I am handling:'
 		console.log( $countdownTimerMsg );
 	}
 }
+
+/**
+ * Find and process countdown timer elements whose components have been marked with the class
+ * attribute.
+ * 
+ * @param {string} selectorStr - Selector string for isolating countdown timer elements within the
+ *     DOM.
+ *
+ * @return {(undefined|jQuery)} Either a jQuery object representing the elements identified by class
+ *     as countdown timers or undefined if an error condition was encountered.
+ */
+// TODO: function processCountdownTimersByClass( … ) { … }
 
 /**
  * Show a countdown timer that was marked by its ID attribute.

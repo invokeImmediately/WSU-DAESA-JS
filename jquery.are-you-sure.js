b27e0a9a2366d7d941dfd7ce46891f7aca1f7bcc
Daniel Rieck, Ph.D <danielcrieck@gmail.com>
2020-04-23 11:30:06 -0700

    Fix bugs with gravity forms involving missing dirty flags
    Fix the following cases with gravity forms where the dirty flag is never applied to the form when it should be: failed form validations, movement to a new page of a paginated form, and movement to a previously dirty page of a paginated form.

--- jquery.are-you-sure.js
+++ jquery.are-you-sure.js
@@ -2,7 +2,7 @@
  * jQuery.are-you-sure.js: Application of Are-You-Sure jQuery Plugin to WSU OUE websites. Please see
  *     https://github.com/codedance/jquery.AreYouSure/ for more details.
  * Author:  Daniel Rieck (danielcrieck@gmail.com) [https://github.com/invokeImmediately]
- * Version: 2.0.0
+ * Version: 2.1.0
  *
  * Published under the MIT license.
  * https://opensource.org/licenses/MIT
@@ -10,6 +10,13 @@
 ( function( $ ) {
 
 var thisFileName = 'jquery.are-you-sure.js';
+var dirtyFormIndicator = 'dirty';
+
+function assertAreYouSureLoaded() {
+	if ( !$.fn.areYouSure ) {
+		throw 'The Are-You-Sure jQuery plugin is missing; please verify that you included it as a build dependency.';
+	}
+}
 
 // Code executed after the browser loads the DOM.
 $( function() {
@@ -20,16 +27,34 @@ $( function() {
 	try {
 		assertAreYouSureLoaded();
 		$gForms = $( '.gform_wrapper > form' );
-		$gForms.areYouSure();		
+		$gForms.areYouSure( {
+			dirtyClass: dirtyFormIndicator
+		} );		
 	} catch (errorMsg) {
 		$.logError( thisFileName, thisFuncName, thisFuncDesc, errorMsg );
 	}
 } );
 
-function assertAreYouSureLoaded() {
-	if ( !$.fn.areYouSure ) {
-		throw 'The Are-You-Sure jQuery plugin is missing; please verify that you included it as a build dependency.';
+// Code executed after a gravity form is rendered.
+$( document ).on( 'gform_post_render', function ( event, form_id, current_page ) {
+	var thisFuncName = 'Gravity Forms post-render event handler';
+	var thisFuncDesc = 'Code executed after a Gravity Forms form has rendered';
+	const gFormSel = '#gform_' + form_id.toString();
+
+	try {
+		assertAreYouSureLoaded();
+		let $gForm = $( gFormSel );
+		let pg1FilledFields = 0;
+		if ( current_page == 1 ) {
+			let $filledFields = $gForm.find( '.gfield_contains_required .gf-value-entered' );
+			pg1FilledFields = $filledFields.length;
+		}
+		if ( current_page > 1 || pg1FilledFields > 0 ) {
+			$gForm.addClass( dirtyFormIndicator );
+		}
+	} catch ( errorMsg ) {
+		$.logError( thisFileName, thisFuncName, thisFuncDesc, errorMsg );
 	}
-}
+} );
 
 } )( jQuery );

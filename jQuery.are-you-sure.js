9726f5ef1d533974ae2370c54640fa522e55bdb6
invokeImmediately <danielcrieck@gmail.com>
2022-03-04 12:06:44 -0800

    Feat: Add bypass to dirty form invalidation check
    Add a mechanism to jQuery.are-you-sure.js that uses a CSS class to bypass the Are-You-Sure-mediated dirty form invalidation checking mechanism for WSUWP forms built with the Gravity Forms plugin.

--- jQuery.are-you-sure.js
+++ jQuery.are-you-sure.js
@@ -1,15 +1,19 @@
-/*!*************************************************************************************************
- *    █ ▄▀▀▄ █  █ █▀▀▀ █▀▀▄ █  █   █▀▀▀ ▄▀▀▄ █▀▀▄ ▐▀▄▀▌▄▀▀▀      █ ▄▀▀▀
- * ▄  █ █  █ █  █ █▀▀  █▄▄▀ ▀▄▄█   █▀▀▀ █  █ █▄▄▀ █ ▀ ▌▀▀▀█   ▄  █ ▀▀▀█
- * ▀▄▄█  ▀█▄  ▀▀  ▀▀▀▀ ▀  ▀▄▄▄▄▀ ▀ ▀     ▀▀  ▀  ▀▄█   ▀▀▀▀  ▀ ▀▄▄█ ▀▀▀ 
+/*-*************************************************************************************************
+ *    █ ▄▀▀▄ █  █ █▀▀▀ █▀▀▄ █  █   ▄▀▀▄ █▀▀▄ █▀▀▀    █  █ ▄▀▀▄ █  █
+ * ▄  █ █  █ █  █ █▀▀  █▄▄▀ ▀▄▄█   █▄▄█ █▄▄▀ █▀▀  ▀▀ ▀▄▄█ █  █ █  █ ▀▀
+ * ▀▄▄█  ▀█▄  ▀▀  ▀▀▀▀ ▀  ▀▄▄▄▄▀ ▀ █  ▀ ▀  ▀▄▀▀▀▀    ▄▄▄▀  ▀▀   ▀▀
+ *
+ *   ▄▀▀▀ █  █ █▀▀▄ █▀▀▀      █ ▄▀▀▀
+ *   ▀▀▀█ █  █ █▄▄▀ █▀▀    ▄  █ ▀▀▀█
+ *   ▀▀▀   ▀▀  ▀  ▀▄▀▀▀▀ ▀ ▀▄▄█ ▀▀▀
  *
  * Application of Are-You-Sure jQuery Plugin to WSU DAESA websites.
  *
- * @version 2.1.1
+ * @version 2.2.0
  *
  * @author Daniel Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)
- * @link https://github.com/invokeImmediately/WSU-DAESA-JS/blob/master/jQuery.are-you-sure.js
- * @license MIT - Copyright (c) 2021 Washington State University
+ * @link https://github.com/invokeImmediately/WSU-DAESA-JS/blob/main/jQuery.are-you-sure.js
+ * @license MIT - Copyright (c) 2022 Washington State University
  *   Permission is hereby granted, free of charge, to any person obtaining a copy of this software
  *     and associated documentation files (the “Software”), to deal in the Software without
  *     restriction, including without limitation the rights to use, copy, modify, merge, publish,
@@ -24,19 +28,40 @@
  *     FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  **************************************************************************************************/
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// TABLE OF CONTENTS
+// -----------------
+//   §1: Persistent documentation for final output..............................................40
+//   §2: Functions for checking form state......................................................56
+//   §3: Set up Are-You-Sure upon page load.....................................................73
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// §1: PERSISTENT DOCUMENTATION for final output
+
+/*!***
+ * jQuery.are-you-sure.js - v2.2.0
+ * Application of Are-You-Sure jQuery Plugin to WSU DAESA websites.
+ * By Daniel C. Rieck (daniel.rieck@wsu.edu). See [GitHub](https://github.com/invokeImmediately/WSU-DAESA-JS/blob/main/jQuery.daesa-custom.js) for more info.
+ * Copyright (c) 2022 Washington State University and governed by the MIT license.
+ ****/
+
 ( function( $ ) {
 
 // Define script's execution context
-var thisFileName = 'jquery.are-you-sure.js';
-var dirtyFormIndicator = 'dirty';
+const thisFileName = 'jquery.are-you-sure.js';
+const dirtyFormIndicator = 'dirty';
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// §2: Functions for CHECKING FORM STATE
 
 function checkLoadedForm( $gForm, current_page ) {
 	if ( !$.isJQueryObj( $gForm ) || $gForm.hasClass( dirtyFormIndicator ) ) {
 		return;
 	}
-	var pg1FilledFields = 0;
+	let pg1FilledFields = 0;
 	if ( current_page == 1 ) {
-		var $filledFields = $gForm.find( '.gfield_contains_required .gf-value-entered' );
+		const $filledFields = $gForm.find( '.gfield_contains_required .gf-value-entered' );
 		pg1FilledFields = $filledFields.length;
 	}
 	if ( current_page > 1 || pg1FilledFields > 0 ) {
@@ -44,10 +69,13 @@ function checkLoadedForm( $gForm, current_page ) {
 	}
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// §3: SET UP ARE-YOU-SURE upon page load
+
 // Use Gravity Forms' post-rendering event as a hook for setting up form invalidation
 $( document ).on( 'gform_post_render', function ( event, form_id, current_page ) {
-	var thisFuncName = 'Gravity Forms post-render event handler';
-	var thisFuncDesc = 'Code executed after a Gravity Forms form has rendered';
+	const thisFuncName = 'Gravity Forms post-render event handler';
+	const thisFuncDesc = 'Code executed after a Gravity Forms form has rendered';
 	try {
 
 		// Make sure thejQuery plugin required for form invalidation has been loaded.
@@ -58,8 +86,11 @@ $( document ).on( 'gform_post_render', function ( event, form_id, current_page )
 		}
 
 		// Set up automatic form invalidation on the form
-		var gFormSel = '#gform_' + form_id.toString();
-		var $gForm = $( gFormSel );
+		const gFormSel = '#gform_' + form_id.toString();
+		const $gForm = $( gFormSel );
+		if ( $gForm.hasClass( "gf-ignore-dirty" ) ) {
+			return;
+		}
 		$gForm.areYouSure( {
 			dirtyClass: dirtyFormIndicator
 		} );

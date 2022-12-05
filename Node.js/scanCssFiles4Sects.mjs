c584403d782785544eb3794c79708af1c0a7c927
invokeImmediately <danielcrieck@gmail.com>
2022-12-04 23:00:20 -0800

    Docs: scanCssFiles4Sects.mjs → Correction
    In «Node.js/scannCssFiles4Sects.mjs», correct link to the file's location on GitHub that is present in the file header comment.

--- Node.js/scanCssFiles4Sects.mjs
+++ Node.js/scanCssFiles4Sects.mjs
@@ -11,10 +11,10 @@
  *
  * The module is designed to quickly extract file sections and compare the sections present in two files.
  *
- * @version 0.0.0
+ * @version 0.0.1
  *
  * @author Daniel C. Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)
- * @link https://github.com/invokeImmediately/WSU-DAESA-JS/blob/main/jQuery.daesa-custom.js
+ * @link https://github.com/invokeImmediately/WSU-DAESA-JS/blob/main/Node.js/scanCssFiles4Sects.mjs
  * @license MIT - Copyright (c) 2022 Washington State University
  *   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.

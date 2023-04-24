406af07e177e6e33c68aa576f84714dece52edb4
invokeImmediately <danielcrieck@gmail.com>
2023-04-24 11:18:15 -0700

    Docs: WSUWP page data extractor
    Update inline documentation in the WSUWP page data extractor «Dev-Tools/extractWpPageDetails.devTools.js» by editing the name of the file at the beginning of the header comment and the link tag.

--- Dev-Tools/extractWpPageDetails.devTools.js
+++ Dev-Tools/extractWpPageDetails.devTools.js
@@ -1,7 +1,7 @@
 /*!*****************************************************************************
- * PrintWp █▀▀▄ ▄▀▀▄ █▀▀▀ █▀▀▀ █▀▀▄ █▀▀▀▐▀█▀▌▄▀▀▄ ▀█▀ █    ▄▀▀▀ · · · · · · · ·
- *  · · ·  █▄▄▀ █▄▄█ █ ▀▄ █▀▀  █  █ █▀▀   █  █▄▄█  █  █  ▄ ▀▀▀█  · · · · · · ·
- * · · · · █    █  ▀ ▀▀▀▀ ▀▀▀▀ ▀▀▀  ▀▀▀▀  █  █  ▀ ▀▀▀ ▀▀▀  ▀▀▀  .devTools.js  ·
+ * extractWp █▀▀▄ ▄▀▀▄ █▀▀▀ █▀▀▀ █▀▀▄ █▀▀▀▐▀█▀▌▄▀▀▄ ▀█▀ █    ▄▀▀▀ · · · · · · ·
+ *  · · · ·  █▄▄▀ █▄▄█ █ ▀▄ █▀▀  █  █ █▀▀   █  █▄▄█  █  █  ▄ ▀▀▀█  · · · · · · ·
+ * · · · · · █    █  ▀ ▀▀▀▀ ▀▀▀▀ ▀▀▀  ▀▀▀▀  █  █  ▀ ▀▀▀ ▀▀▀  ▀▀▀  .devTools.js
  * ·············································································
  *
  * Browser dev tools script that leverages jQuery to quickly extract data about the page-type posts that are present in a WSUWP website's administration area.
@@ -11,7 +11,7 @@
  * @version 0.1.1
  *
  * @author Daniel C. Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)
- * @link https://github.com/invokeImmediately/WSU-DAESA-JS/blob/main/printWpPageDetails.devTools.js
+ * @link https://github.com/invokeImmediately/WSU-DAESA-JS/blob/main/Dev-Tools/extractWpPageDetails.devTools.js
  * @license MIT - Copyright (c) 2023 Washington State University
  *   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.

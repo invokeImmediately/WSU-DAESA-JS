48e60dac73c1c33ad2bce56b587cc7379beb1162
invokeImmediately <danielcrieck@gmail.com>
2023-07-26 18:24:44 -0700

    Feat: DevTools script → WDS cons6t extractor
    Add «DevTools/extractWdsConstiuentsFromGH.js» to the project as a web browsers
     DevTools utility script. Write the script to extract the latest lists of WSU
     Web Design System (WDS) elements, components, and modules from the WDS
     project's repository on GitHub.

---
+++ DevTools/extractWdsConstituentsFromGH.js
@@ -0,0 +1,147 @@
+/*!*****************************************************************************
+ * ▓▓▓▒ █▀▀▀▐▄ ▄▌▐▀█▀▌█▀▀▄ ▄▀▀▄ ▄▀▀▀▐▀█▀▌▐   ▌█▀▀▄ ▄▀▀▀ ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+ * ▓▓▒▒ █▀▀   █    █  █▄▄▀ █▄▄█ █     █  ▐ █ ▌█  █ ▀▀▀█ ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+ * ▓▒▒▒ ▀▀▀▀▐▀ ▀▌  █  ▀  ▀▄█  ▀  ▀▀▀  █   ▀ ▀ ▀▀▀  ▀▀▀   ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓
+ * ▒▒▒▒▒                                                  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓
+ * ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ ConstituentsFromGH.js ▒▒▒▒▒▒▒▒▒▓▓▓
+ *
+ * Web browser DevTools code snippet that automatically collects the latest
+ *  lists of WSU Web Design System elements, components, and modules from the
+ *  project's repository on GitHub.
+ *
+ * @author danielcrieck@gmail.com
+ *  <danielcrieck@gmail.com>
+ *  (https://github.com/invokeImmediately)
+ *
+ * @link https://github.com/invokeImmediately/WSU-DAESA-JS…
+ *  …/blob/main/Chrome-Snippets/github-extract-wds-constituents.js
+ *
+ * @license MIT — Copyright 2021. by Washington State University
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to
+ *   deal in the Software without restriction, including without limitation the
+ *   rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ *   sell copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *  The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *   DEALINGS IN THE SOFTWARE.
+ */
+
+( function () {
+  'use strict';
+  
+  const wdsGhUrls = {
+    elements:   'https://github.com/wsuwebteam/web-design-system/tree/main/sr' +
+      'c/elements',
+    components: 'https://github.com/wsuwebteam/web-design-system/tree/main/sr' +
+      'c/components',
+    modules:    'https://github.com/wsuwebteam/web-design-system/tree/main/sr' +
+      'c/modules',
+  };
+
+  const ghDomainDest6n = 'https://github.com/wsuwebteam/';
+
+  async function checkDomainIsOnGitHub() {
+    const curLocation = window.location.href;
+    const onGitHub = curLocation.match( /https:\/\/github.com/ );
+    if( !onGitHub ) {
+      console.log( 'Navigating to the GitHub domain for data extraction.' );
+      await waitFor( 667 );
+      window.location = ghDomainDest6n;
+    }
+    return onGitHub;
+  }
+
+  async function extractWdsConstituents() {
+    let wdsConstituents = '';
+    wdsConstituents = await extractWdsCon7tFromGhRepo(
+      'Elements', wdsGhUrls.elements, wdsConstituents
+    );
+    wdsConstituents = await extractWdsCon7tFromGhRepo(
+      'Components', wdsGhUrls.components, wdsConstituents
+    );
+    wdsConstituents = await extractWdsCon7tFromGhRepo(
+      'Modules', wdsGhUrls.modules, wdsConstituents
+    );
+    return wdsConstituents;
+  }
+
+  async function extractWdsCon7tFromGhRepo( con7tType, con7tUrl, wdsCon7ts ) {
+    const finalResponse = await fetchUrl( con7tUrl );
+    const ghData = JSON.parse( finalResponse );
+    const numItems = ghData.payload.tree.items.length;
+    let treeChar = '├';
+    if ( wdsCon7ts != '' ) {
+      wdsCon7ts += '\n';
+    }
+    wdsCon7ts += `${con7tType} (×${numItems})\n`;
+    ghData.payload.tree.items.forEach( ( item, index ) => {
+      if ( index == numItems - 1 ) {
+        treeChar = '└'
+      }
+      wdsCon7ts += `  ${treeChar}─${item.name}\n`;
+    } );
+    return wdsCon7ts;
+  }
+
+  async function fetchUrl( url ) {
+    let finalResponse = null;
+    await fetch( url )
+      .then( ( response ) => {
+        if ( !response.ok ) {
+          throw new Error( `Unable to access resource « ${url} ».` );
+        }
+        return response.text();
+      } )
+      .then( ( response ) => {
+        finalResponse = response;
+      } );
+    return finalResponse;
+  }
+
+  async function main() {
+    try {
+      if ( !( await checkDomainIsOnGitHub() ) ) {
+        return;
+      }
+      const wdsConstituents = await extractWdsConstituents();
+      outputWdsConstituents( wdsConstituents );
+    } catch( error ) {
+      console.error( error );
+    }
+  }
+
+  async function outputWdsConstituents( wdsConstituents ) {
+    console.log(
+`Ready to write output to clipboard. Please close DevTools within 1.5 seconds.`
+    );
+    await waitFor( 1500 );
+    wdsConstituents =
+`Constituents of @github/wsuwebteam/web-design-system as of ${Date()}:\n\n`
+      + wdsConstituents;
+    await navigator.clipboard.writeText( wdsConstituents );
+    console.log(
+`The following output was stored to clipboard via the Clipboard API:
+──────────────────────────────────────────────────────────────────
+
+${wdsConstituents}`
+    );
+  }
+
+  function waitFor( timeInMs ) {
+    return new Promise( ( resolve ) => {
+      setTimeout( () => {
+        resolve( '' );
+      }, timeInMs );
+    } );
+  }
+
+  main();
+} )();

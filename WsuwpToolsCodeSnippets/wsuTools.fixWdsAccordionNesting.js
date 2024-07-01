8d6e02105cf95abded4b8e51b1af3757a499cee2
invokeImmediately <danielcrieck@gmail.com>
2024-07-01 14:35:44 -0700

    FEAT: WSUWP Tools Snippet → Fix nested accordions
    Add «WsuwpToolsCodeSnippets/wsuTools.fixWdsAccordionNesting.js» as a
      script that provides missing collapse functionality to nested
      accordions utilized on WDS 2 or 3 themed WSUWP-hosted websites.
      Design the script for application to WSUWP websites via the WSUWP
      Tools plugin as a custom JS code snippet.
    
    Write the script to leverage jQuery to set up click event handlers on
      the toggle button of nested accordions. Write the handlers to check
      that the expansion state of an accordion has been properly toggled
      upon user interaction and intervene if necessary.

---
+++ WsuwpToolsCodeSnippets/wsuTools.fixWdsAccordionNesting.js
@@ -0,0 +1,121 @@
+/*!***
+ * ▓▒▒▒ wsuTools.fixWds ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▒▒▓▒▒▓▒▒▓▓▒▒▓▓▒▒▓▓▒▒▓▓▓▓▓
+ * ▓▒▒▒  ▄▀▀▄ ▄▀▀▀ ▄▀▀▀ ▄▀▀▄ █▀▀▄ █▀▀▄ ▀█▀ ▄▀▀▄ ▐▀▀▄ ▒▒▓▒▒▓▒▒▓▒▒▓▓▒▒▓▓▒▒▓▓▒▒▓▓▓▓
+ * ▓▓▒▒  █▄▄█ █    █    █  █ █▄▄▀ █  █  █  █  █ █  ▐ ▒▒▓▒▒▓▒▒▓▒▒▓▓▒▒▓▓▒▒▓▓▒▒▓▓▓▓
+ * ▓▓▒▒  █  ▀  ▀▀▀  ▀▀▀  ▀▀  ▀  ▀▄▀▀▀  ▀▀▀  ▀▀  ▀  ▐ ▒▒▒▓▒▒▓▒▒▓▒▒▓▓▒▒▓▓▒▒▓▓▒▒▓▓▓
+ * ▓▓▓▒  Nesting.mjs ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▒▒▓▒▒▓▒▒▓▓▒▒▓▓▒▒▓▓▒▒▓▓▓
+ *
+ * wsuTools.fixWdsAccordionNesting.js - v0.0.0
+ *
+ * Provide missing collapse functionality to nested accordions used on WDS 2 or
+ *   3 themed WSUWP websites.
+ *
+ * By Daniel C. Rieck (daniel.rieck@wsu.edu)
+ *   [https://github.com/invokeImmediately/]
+ *
+ * Copyright (c) 2024 Washington State University
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the “Software”), to
+ *   deal in the Software without restriction, including without limitation the
+ *   rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ *   sell copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *   DEALINGS IN THE SOFTWARE.
+ ****/
+
+(function($) {
+
+  /**
+   * Use CSS classes to collapse an expanded WDS accordion component.
+   */
+  function collapseNestedAccrdn($accordion) {
+    if ($accordion.hasClass('wsu-accordion--open')) {
+      $accordion.removeClass('wsu-accordion--open');
+    }
+  }
+
+  /**
+   * Signal to screen readers that a tested accordion has been expanded via
+   *   WAI-ARIA.
+   */
+  function sgnlExpandedNestedAccrdn($toggle) {
+    if ($toggle.attr('aria-expanded') != 'true') {
+      $toggle.attr('aria-expanded', 'true');
+    }
+  }
+
+  /**
+   * Enhance event handling to fix issues with collapsible behavior of WDS
+   *   accordion components. Based on testing, it is only click events that are
+   *   broken in WDS 2 or 3.
+   */
+  function fixNestedAccrdnTggls($main) {
+    // ·> Set up click event handling on WDS accordions nested inside of other
+    // ·<   accordions
+    $main.on(
+      'click',
+      '.wsu-accordion .wsu-accordion .wsu-accordion--toggle',
+      function() {
+        const $toggle = $(this);
+        $prntAccrdn = $toggle.parents('.wsu-accordion').first();
+        if ($prntAccrdn.hasClass('wsu-accordion--open')) {
+            setTimeout(collapseNestedAccrdn, 200, $prntAccrdn);
+        } else {
+            setTimeout(sgnlExpandedNestedAccrdn, 200, $toggle);
+        }
+      }
+    );
+
+    // ·> Set up click event handling on accordions nested inside of WDS card
+    // ·<   blocks.
+    $main.on(
+      'click',
+      '.wsu-card .wsu-accordion .wsu-accordion--toggle',
+      function() {
+        const $toggle = $(this);
+        $prntAccrdn = $toggle.parents('.wsu-accordion').first();
+        if ($prntAccrdn.hasClass('wsu-accordion--open' )) {
+            setTimeout(collapseNestedAccrdn, 200, $prntAccrdn);
+        } else {
+            setTimeout(sgnlExpandedNestedAccrdn, 200, $toggle);
+        }
+      }
+    );
+
+    // ·> Set up click event handling on child elements of accordion toggle
+    // ·<   buttons.
+    $main.on(
+      'click',
+      '.wsu-accordion .wsu-accordion--toggle strong',
+      function() {
+        const $toggle = $(this).parents('.wsu-accordion--toggle').first();
+        $prntAccrdn = $toggle.parents('.wsu-accordion').first();
+        $toggle.trigger('click');
+      }
+    );
+  }
+  
+  /**
+   * Begin IIFE execution by checking for nested accordions, whose interactive
+   *  behavior currently needs to be fixed through JS intervention.
+   */
+  function iifeMain() {
+    $('#wsu-content').each(function() {
+        const $main = $(this);
+        fixNestedAccrdnTggls($main);
+    });
+  }
+
+  iifeMain();
+})(jQuery);
